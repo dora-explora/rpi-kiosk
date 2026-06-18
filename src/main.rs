24cea6da759a7f23b6d3947fce90d473032e4515
dora-explora <sarrafdara@gmail.com>
2026-06-18 00:15:16 -0700

    add clock

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,8 @@ use std::{
     time::Duration
 };
 
-use embedded_graphics::{draw_target::DrawTarget, geometry::Dimensions, pixelcolor::Rgb888, prelude::*, primitives::{Primitive, PrimitiveStyle, Rectangle}};
+use chrono::Local;
+use embedded_graphics::{prelude::*, draw_target::DrawTarget, mono_font::{MonoTextStyle, jis_x0201::FONT_9X15}, pixelcolor::Rgb888, primitives::{Primitive, PrimitiveStyle, Rectangle}, text::Text};
 use linuxfb::{Framebuffer, set_terminal_mode, TerminalMode};
 
 mod display;
@@ -16,33 +17,44 @@ const WIDTH: usize = 320;
 const HEIGHT: usize = 240;
 
 fn main() -> io::Result<()> {
-    let fbdev = Framebuffer::new("/dev/fb1").expect("could not open /dev/fb1 as framebuffer");
-    let mmap = fbdev.map().expect("could not open framebuffer memmap");
-    let mut tft = display::TFT::new(mmap);
-
+    // set tty1 to graphics mode (if possible)
     match File::open("/dev/tty1") {
         Ok(tty) => _ = set_terminal_mode(&tty, TerminalMode::Graphics),
         Err(_) => {},
     }
 
+    // open fb1 and grab the corresponding mmap (basically mut [u8])
+    let fbdev = Framebuffer::new("/dev/fb1").expect("could not open /dev/fb1 as framebuffer");
+    let mmap = fbdev.map().expect("could not open framebuffer memmap");
+    let mut tft = display::TFT::new(mmap); // construct new tft from it
+
+    // make new input data struct and arc-mutex
     let mut input = input::Input {t: false, x: 0, y: 0, p: 0};
     let inputam = Arc::new(Mutex::new(input.clone()));
 
+    // spawn input thread with that arc-mutex
     let inputamclone = inputam.clone();
     spawn(move || input::input(inputamclone));
 
+    // main rendering loop
+    let style = MonoTextStyle::new(&FONT_9X15, Rgb888::WHITE);
     loop {
+        // try to get up-to-date input information
         match inputam.try_lock() {
-            Err(Poisoned(_)) => {}, // println!("poisoned error {e} from input thread"),
-            Err(WouldBlock) => {}, // println!("would block error from input thread"),
+            Err(_) => {}, // an error just means that the other thread is using it currently
+            // Err(Poisoned(e)) => println!("poisoned error {e} from input thread"),
+            // Err(WouldBlock) => println!("would block error from input thread"),
             Ok(g) => {input.t = g.t; input.x = g.x; input.y = g.y; input.p = g.p}
         }
 
-        tft.fb.clear(Rgb888::new(22, 15, 28));
+        tft.fb.clear(Rgb888::BLACK);
+
+        let time = Local::now().time();
+        Text::new(&time.format("%-I:%M:%S %p").to_string().as_str(), Point::new(4, 14), style).draw(&mut tft.fb).unwrap();
         tft.fb.bounding_box().into_styled(PrimitiveStyle::with_stroke(Rgb888::WHITE, 4)).draw(&mut tft.fb).unwrap();
 
         tft.flush();
-        sleep(Duration::from_millis(10));
+        sleep(Duration::from_millis(300));
     }
     // return Ok(());
 }
